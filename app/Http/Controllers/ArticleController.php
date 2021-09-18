3286a32e7a61dc55838f7550b8046301dca0cb47
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-18 22:43:05 +0900

    index.blade.php作成済み

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $art = Article::find();
+        return view('art.index', ['art' => $art]);
+    }
 }
