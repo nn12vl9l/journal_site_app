6ec2363587ac36250c5bfc605e048255579a0c68
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-18 22:35:32 +0900

    controller作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles', [App\Http\Controllers\ArticleController::class, 'index']);
+Route::get('/articles/{id}', [App\Http\Controllers\ArticleController::class, 'show']);
